d7bd7fd8f93bfa7c6848d94e89e7f65402f60837
hnrainll <hnrain1004@gmail.com>
2014-05-13 22:45:31 +0800

    net request update

--- src/com/leochin/findfriends/util/Utility.java
+++ src/com/leochin/findfriends/util/Utility.java
@@ -15,7 +15,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
 
 /**
  * 
@@ -27,18 +26,19 @@ import android.util.Log;
 
 public class Utility {
 
-	public static SharedPreferences getSharedPreferences(Context context, String name){
+	public static SharedPreferences getSharedPreferences(Context context,
+			String name) {
 
 		SharedPreferences sp = context.getSharedPreferences(name,
 				Context.MODE_PRIVATE);
 		return sp;
 	}
-	
-	public static Editor getEditor(Context context, String name){
-		
-		return getSharedPreferences(context, name).edit();	
+
+	public static Editor getEditor(Context context, String name) {
+
+		return getSharedPreferences(context, name).edit();
 	}
-	
+
 	public static void setSharedInt(Context context, String name, String key,
 			int value) {
 
@@ -60,11 +60,12 @@ public class Utility {
 		return getSharedPreferences(context, name).getInt(key, 0);
 	}
 
-	public static String getSharedString(Context context, String name, String key) {
+	public static String getSharedString(Context context, String name,
+			String key) {
 
 		return getSharedPreferences(context, name).getString(key, "");
 	}
-	
+
 	/**
 	 * 判断当前是否联网 需要添加权限：<uses-permission
 	 * android:name="android.permission.ACCESS_NETWORK_STATE" />
@@ -82,43 +83,55 @@ public class Utility {
 
 		return (info != null) && (info.isConnectedOrConnecting());
 	}
-	
+
 	/**
 	 * httpPostRequest请求，成功返回数据，失败返回为空的String
 	 */
-	public static String httpPostRequest(String url , List<NameValuePair> params){
-	    
-        String resultString = "";
-	    
-	    try {
+	public static String httpPostRequest(String url, List<NameValuePair> params) {
 
-	        HttpPost httpRequest = new HttpPost(url); // 使用Apache的网络库
-	        
-            httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
-            
-            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
-            Debugs.d("leochin", "url="+url +"--1->"+httpResponse.getStatusLine());
-            if (httpResponse.getStatusLine().getStatusCode() == 200) {
-                resultString = EntityUtils.toString(httpResponse.getEntity()); // 服务器返回的数据
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-	    return resultString;
+		String resultString = "";
+
+		try {
+
+			HttpPost httpRequest = new HttpPost(url); // 使用Apache的网络库
+
+			httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+
+			HttpResponse httpResponse = new DefaultHttpClient()
+					.execute(httpRequest);
+			Debugs.d("leochin",
+					"url=" + url + "--1->" + httpResponse.getStatusLine());
+			if (httpResponse.getStatusLine().getStatusCode() == 200) {
+				resultString = EntityUtils.toString(httpResponse.getEntity()); // 服务器返回的数据
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultString;
 	}
-	
-	/**
-     * httpPostRequest请求，成功返回数据，失败返回为空的String
-     */
-    public static void httpPostRequestNoBack(String url , List<NameValuePair> params){
-        try {
 
-            HttpPost httpRequest = new HttpPost(url); // 使用Apache的网络库
-            httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
-            HttpResponse rp = new DefaultHttpClient().execute(httpRequest);
-            Debugs.d("leochin", "url="+url +"--2->"+rp.getStatusLine());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * httpPostRequest请求，成功返回数据，失败返回为空的String
+	 */
+	public static void httpPostRequestNoBack(final String url,
+			final List<NameValuePair> params) {
+
+		new Thread() {
+			public void run() {
+				try {
+
+					HttpPost httpRequest = new HttpPost(url); // 使用Apache的网络库
+					httpRequest.setEntity(new UrlEncodedFormEntity(params,
+							HTTP.UTF_8));
+					HttpResponse rp = new DefaultHttpClient()
+							.execute(httpRequest);
+					Debugs.d("leochin",
+							"url=" + url + "--2->" + rp.getStatusLine());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+
+	}
 }
